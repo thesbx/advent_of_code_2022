ef96addd7a73dc253393754c2cce4bf836e031ae
Matt Miller <mattmillerf@pm.me>
2022-12-02 10:35:36 -0500

    complete part 2

--- day2/day2.py
+++ day2/day2.py
@@ -4,7 +4,7 @@ lines = guide.read()
 
 rounds = [[line for line in round.split(" ")] for round in lines.split("\n")]
 
-openent_scores = {
+rock_paper_scissors = {
     "A": 1,
     "B": 2,
     "C": 3,
@@ -17,7 +17,7 @@ our_scores = {
 }
 
 def get_winner(round):
-    openent_score = openent_scores[round[0]]
+    openent_score = rock_paper_scissors[round[0]]
     our_score = our_scores[round[1]]
 
     if openent_score == 1 and our_score == 2 or openent_score == 2 and our_score == 3 or openent_score == 3 and our_score == 1:
@@ -41,4 +41,40 @@ scores = []
 for round in rounds:
     scores.append(get_winner(round))
 
-print(sum(scores))
+print(f"Answer to round #1: {sum(scores)}")
+
+# Round 2
+
+guide = {
+    "X": 0,
+    "Y": 3,
+    "Z": 6,
+}
+
+def determine_score(round):
+    result = guide[round[1]]
+    opponent_score = rock_paper_scissors[round[0]]
+    if opponent_score == 1 and result == 6:
+        return 2 + result
+    elif opponent_score == 2 and result == 6:
+        return 3 + result
+    elif opponent_score == 3 and result == 6:
+        return 1 + result
+    elif opponent_score == 1 and result == 3:
+        return 1 + result
+    elif opponent_score == 2 and result == 3:
+        return 2 + result
+    elif opponent_score == 3 and result == 3:
+        return 3 + result
+    elif opponent_score == 1 and result == 0:
+        return 3 + result
+    elif opponent_score == 2 and result == 0:
+        return 1 + result
+    elif opponent_score == 3 and result == 0:
+        return 2 + result
+
+new_scores = []
+for round in rounds:
+    new_scores.append(determine_score(round))
+
+print(sum(new_scores))
