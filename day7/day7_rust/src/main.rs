7284f98aceb9af9305ec1c1c7c264fa234a6f061
Matt Miller <mattmillerf@pm.me>
2022-12-07 09:58:26 -0500

    day7 begins

---
+++ day7/day7_rust/src/main.rs
@@ -0,0 +1,35 @@
+use std::fs;
+
+fn parse_file(filename: &str) -> Vec<String> {
+    let file = fs::read_to_string(filename).expect("Couldn't read file");
+    let mut tree = Vec::new();
+    for content in file.split("\n") {
+        for string in content.split_whitespace() {
+            tree.push(string.to_string());
+        }
+    }
+    tree
+}
+
+fn process_commands(input: Vec<String>) {
+    let mut current_dir = String::new(); 
+    let mut parent_dir = String::new();
+
+    for line in input {
+       if line.contains("$ cd ") {
+           let dir = &line[5..];
+           if dir == ".." {
+               current_dir = &parent_dir;
+           } else {
+               parent_dir = &current_dir;
+               current_dir = &current_dir + "/" + &dir;
+           }
+               
+       }
+    }
+}
+
+fn main() {
+    let commands = parse_file("./test.txt");
+    println!("{:?}", commands);
+}
