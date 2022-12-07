8d15064df2904184f7e851a90b26b2d9ec2c4920
Matt Miller <mattmillerf@pm.me>
2022-12-07 14:56:56 -0500

    copy Prime's code

--- day7/day7_rust/src/main.rs
+++ day7/day7_rust/src/main.rs
@@ -1,37 +1,74 @@
-use std::fs;
+use anyhow::Result;
 
-fn parse_file(filename: &str) -> Vec<String> {
-    let file = fs::read_to_string(filename).expect("Couldn't read file");
-    let mut tree = Vec::new();
-    for content in file.split("\n") {
-        for string in content.split_whitespace() {
-            tree.push(string.to_string());
+fn main() -> Result<()> {
+    let input = std::fs::read_to_string("../input.txt")?;
+
+    let mut stack = vec![
+        ("/", 0)
+    ];
+
+    let mut final_countdown = vec![];
+
+    let total_space = 70000000;
+    let space_to_delete = 30000000;
+
+    let mut total = 0;
+
+    for line in input.lines().filter(|l| !l.is_empty()) {
+        if line == "$ cd /" || line == "$ ls" {
+            continue;
+        }
+
+        if line.starts_with("$ cd ") {
+            let dir = &line[5..];
+            if dir == ".." {
+                let (name, amount) = stack.pop().unwrap();
+                if amount <= 100_000 {
+                    total += amount;
+                }
+                stack.last_mut().unwrap().1 += amount;
+                final_countdown.push((name, amount));
+            } else {
+                stack.push((dir, 0));
+            }
+            continue;
+        }
+
+        let (amount, _) = line.split_once(" ").unwrap();
+
+        if let Ok(amount) = amount.parse::<usize>() {
+            stack.last_mut().unwrap().1 += amount;
+        } else if amount == "dir" {
+            // ignore
         }
     }
-    tree
-}
 
-fn process_commands(input: &Vec<String>) {
-    let mut current_dir = String::new(); 
-    let mut parent_dir = &String::new();
+    while stack.len() > 0 {
+        let (name, amount) = stack.pop().unwrap();
+        final_countdown.push((name, amount));
 
-    for line in input {
-       if line.contains("$ cd ") {
-           let dir = &line[5..];
-           if dir == ".." {
-               current_dir = parent_dir.to_string();
-           } else {
-               parent_dir = &current_dir.to_string();
-               current_dir = current_dir.to_owned() + &String::from("/") + dir;
-           }
-               
-       }
+        if stack.len() > 0 {
+            stack.last_mut().unwrap().1 += amount;
+        }
     }
-    println!("{}", current_dir);
+
+    let free_space = total_space - final_countdown.last().unwrap().1;
+    let space_required = space_to_delete - free_space;
+
+    let total = final_countdown
+        .into_iter()
+        .filter(move |(_, amount)| *amount >= space_required)
+        .map(|(_, amount)| {
+            return amount;
+        })
+        .min();
+
+    println!("total {:?}", total);
+
+    return Ok(());
 }
 
-fn main() {
-    let commands = parse_file("./test.txt");
-    process_commands(&commands);
-    println!("{:?}", commands);
+#[cfg(test)]
+mod test {
+
 }
