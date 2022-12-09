07e0872aabc1cd549358b4e29b32b9a05cc8a464
Matt Miller <mattmillerf@pm.me>
2022-12-09 09:56:42 -0500

    refactor

--- aoc_rust/src/bin/day9.rs
+++ aoc_rust/src/bin/day9.rs
@@ -2,7 +2,7 @@ use anyhow::Result;
 use std::fs;
 
 fn main() -> Result<()> {
-    let input = fs::read_to_string("src/bin/inputs/day9.test")?;
+    let input = fs::read_to_string("inputs/day9.test")?;
     let lines = input
         .split("\n")
         .collect::<Vec<_>>();
