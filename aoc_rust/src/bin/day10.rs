04d500777d982cc2670e3534a576dd6d324b08f8
Matt Miller <mattmillerf@pm.me>
2022-12-10 17:59:44 -0500

    experimenting

--- aoc_rust/src/bin/day10.rs
+++ aoc_rust/src/bin/day10.rs
@@ -3,15 +3,15 @@ use anyhow::Result;
 fn main() -> Result<()> {
     let file = std::fs::read_to_string("inputs/day10.test")?;
     let lines = file.split("\n").collect::<Vec<_>>();
-    let commands = lines.iter().fold();
+    let commands = lines.iter();
     
-   // for line in lines {
+    //for line in lines {
 
-   //     if line.split_once(" ").is_some() {
-   //         let (cmd, n) = line.split_once(" ").unwrap();
-   //         println!("{:?} --- {:?}", cmd, n.parse::<i32>().unwrap());
-   //     }
-   // }
+    //    if line.split_once(" ").is_some() {
+    //        let (cmd, n) = line.split_once(" ").unwrap();
+    //        println!("{:?} --- {:?}", cmd, n.parse::<i32>().unwrap());
+    //    }
+    //}
     
     println!("{:?}", commands);
     return Ok(())
